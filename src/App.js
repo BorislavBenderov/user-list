1768bf3d91127f608fc7f791efe0cfaf81a542c7
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-10-17 11:24:45 +0300

    Create search form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import { Header } from "./Components/Header";
+import { Search } from "./Components/Search";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main className="main">
+      <section className="card users-container">
+        <Search />
+      </section>
+      </main>
     </div>
   );
 }
