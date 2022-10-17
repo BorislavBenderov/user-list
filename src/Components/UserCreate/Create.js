a6610b6bca0f8b9a78abcd9c12563e52da81064c
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-10-17 16:36:02 +0300

    Make create page form controlled

--- src/Components/UserCreate/Create.js
+++ src/Components/UserCreate/Create.js
@@ -1,4 +1,34 @@
+import { useState } from "react";
+
 export const Create = ({ onClose }) => {
+    const [values, setValues] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        imageUrl: '',
+        country: '',
+        city: '',
+        street: '',
+        streetNumber: ''
+    });
+
+    const changeHandler = (e) => {
+        setValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const userdata = Object.fromEntries(new FormData(e.target));
+        console.log(userdata);
+    }
+
+
+
     return (
         <div className="overlay">
             <div className="backdrop" onClick={() => onClose()}/>
@@ -24,7 +54,7 @@ export const Create = ({ onClose }) => {
                             </svg>
                         </button>
                     </header>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <div className="form-row">
                             <div className="form-group">
                                 <label htmlFor="firstName">First name</label>
@@ -32,7 +62,7 @@ export const Create = ({ onClose }) => {
                                     <span>
                                         <i className="fa-solid fa-user" />
                                     </span>
-                                    <input id="firstName" name="firstName" type="text" />
+                                    <input id="firstName" name="firstName" type="text" value={values.firstName} onChange={changeHandler}/>
                                 </div>
                                 <p className="form-error">
                                     First name should be at least 3 characters long!
@@ -44,7 +74,7 @@ export const Create = ({ onClose }) => {
                                     <span>
                                         <i className="fa-solid fa-user" />
                                     </span>
-                                    <input id="lastName" name="lastName" type="text" />
+                                    <input id="lastName" name="lastName" type="text" value={values.lastName} onChange={changeHandler}/>
                                 </div>
                                 <p className="form-error">
                                     Last name should be at least 3 characters long!
@@ -58,7 +88,7 @@ export const Create = ({ onClose }) => {
                                     <span>
                                         <i className="fa-solid fa-envelope" />
                                     </span>
-                                    <input id="email" name="email" type="text" />
+                                    <input id="email" name="email" type="text" value={values.email} onChange={changeHandler}/>
                                 </div>
                                 <p className="form-error">Email is not valid!</p>
                             </div>
@@ -68,7 +98,7 @@ export const Create = ({ onClose }) => {
                                     <span>
                                         <i className="fa-solid fa-phone" />
                                     </span>
-                                    <input id="phoneNumber" name="phoneNumber" type="text" />
+                                    <input id="phoneNumber" name="phoneNumber" type="text" value={values.phoneNumber} onChange={changeHandler}/>
                                 </div>
                                 <p className="form-error">Phone number is not valid!</p>
                             </div>
@@ -79,7 +109,7 @@ export const Create = ({ onClose }) => {
                                 <span>
                                     <i className="fa-solid fa-image" />
                                 </span>
-                                <input id="imageUrl" name="imageUrl" type="text" />
+                                <input id="imageUrl" name="imageUrl" type="text" value={values.imageUrl} onChange={changeHandler}/>
                             </div>
                             <p className="form-error">ImageUrl is not valid!</p>
                         </div>
@@ -90,7 +120,7 @@ export const Create = ({ onClose }) => {
                                     <span>
                                         <i className="fa-solid fa-map" />
                                     </span>
-                                    <input id="country" name="country" type="text" />
+                                    <input id="country" name="country" type="text" value={values.country} onChange={changeHandler}/>
                                 </div>
                                 <p className="form-error">
                                     Country should be at least 2 characters long!
@@ -102,7 +132,7 @@ export const Create = ({ onClose }) => {
                                     <span>
                                         <i className="fa-solid fa-city" />
                                     </span>
-                                    <input id="city" name="city" type="text" />
+                                    <input id="city" name="city" type="text" value={values.city} onChange={changeHandler}/>
                                 </div>
                                 <p className="form-error">
                                     City should be at least 3 characters long!
@@ -116,7 +146,7 @@ export const Create = ({ onClose }) => {
                                     <span>
                                         <i className="fa-solid fa-map" />
                                     </span>
-                                    <input id="street" name="street" type="text" />
+                                    <input id="street" name="street" type="text" value={values.street} onChange={changeHandler}/>
                                 </div>
                                 <p className="form-error">
                                     Street should be at least 3 characters long!
@@ -128,7 +158,7 @@ export const Create = ({ onClose }) => {
                                     <span>
                                         <i className="fa-solid fa-house-chimney" />
                                     </span>
-                                    <input id="streetNumber" name="streetNumber" type="text" />
+                                    <input id="streetNumber" name="streetNumber" type="text" value={values.streetNumber} onChange={changeHandler}/>
                                 </div>
                                 <p className="form-error">
                                     Street number should be a positive number!
@@ -139,7 +169,7 @@ export const Create = ({ onClose }) => {
                             <button id="action-save" className="btn" type="submit">
                                 Save
                             </button>
-                            <button id="action-cancel" className="btn" type="button">
+                            <button id="action-cancel" className="btn" type="button" onClick={() => onClose()}>
                                 Cancel
                             </button>
                         </div>
